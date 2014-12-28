656b276678a525617fa014f9b791206d06de290a
Matt Dodge <matt@neutral.io>
2014-12-28 15:18:43 -0800

    Making it work  - executes git pull on target directory  - checks against secret key

--- index.php
+++ index.php
@@ -1,36 +1,52 @@
 <?php
 
-
 $config = parse_ini_file('config.ini', true);
 
-
 if ($config === false || !is_array($config)) {
-	die("Invalid config file - be sure that you have a 'config.ini' present (not 'config.ini.example') and that any non-alphanumeric values are wrapped in double quotes");
+	die500("Invalid config file - be sure that you have a 'config.ini' present (not 'config.ini.example') and that any non-alphanumeric values are wrapped in double quotes");
 }
 
 try {
-	$payloadBody = json_decode(file_get_contents('php://input'), true);
+	// Keep the raw payload so we can hash it and check the secret key
+	$payload = file_get_contents('php://input');
+
+	// But really, we're going to need a JSON decoded body to access data
+	$payloadBody = json_decode($payload, true);
+
 	if (! $payloadBody ) {
 		throw new Exception("No payload body detected");
 	}
 } catch ( Exception $e ) {
-	die("Could not decode the payload body...something seems fishy");
+	die500("Could not decode the payload body...something seems fishy");
 }
 
 // Make sure that they have changed from the default secret key
 if (strcmp(getConfig('SECRET_PHRASE'), 'CHANGETHISKEY') === 0) {
-	die('Change the default secret phrase - this is a security precaution.');
+	die500('Change the default secret phrase - this is a security precaution.');
 }
 
+// We require a GitHub signature header to validate against the secret key
+if( !array_key_exists( 'HTTP_X_HUB_SIGNATURE', $_SERVER ) ) {
+	die500( 'Missing X-Hub-Signature header.' );
+}
+if ( ! checkSecretKey($payload) ) {
+	die500( 'Secret key does not match' );
+}
+
+// Go through every sink we have configured, see if it's a match
 foreach($config as $configName => $configArr) {
+	
+	// Don't process the config settings
 	if (strcmp($configName, 'CONFIG') === 0) {
-		// Don't process the config settings
 		continue;
 	}
 
 	if (configMatches($configArr, $payloadBody)) {
-		echo "We have a match..." . $configName;
-		processConfig($configArr);
+		try {
+			processConfig($configArr);
+		} catch ( Exception $e ) {
+			die500("Matched a sink, but could not process:\n" . $e->getMessage());
+		}
 	}
 }
 
@@ -39,9 +55,9 @@ foreach($config as $configName => $configArr) {
  */
 function configMatches($cfg, $payload) {
 	try {
-		return strcasecmp($cfg['GITHUB_ACCOUNT'], $payload['repository']['owner']['login']) === 0 &&
-			strcasecmp($cfg['GITHUB_REPO'], $payload['repository']['name']) === 0 &&
-			strcasecmp('refs/heads/' . $cfg['GITHUB_BRANCH'], $payload['ref']) === 0;
+		return !(strcasecmp($cfg['GITHUB_ACCOUNT'], $payload['repository']['owner']['name']) ||
+			strcasecmp($cfg['GITHUB_REPO'], $payload['repository']['name']) ||
+			strcasecmp('refs/heads/' . $cfg['GITHUB_BRANCH'], $payload['ref']));
 
 	} catch ( Exception $e ) {
 		return false;
@@ -49,10 +65,77 @@ function configMatches($cfg, $payload) {
 }
 
 /**
+ * Process a sink config for syncing.
+ *
+ * Raise an exception if anything goes wrong.
  *
  */
 function processConfig($cfg) {
+	$curDir = getcwd();
+	$targetDir = getFromArray('DIRECTORY', $cfg, '.');
 
+	if (is_dir($targetDir)) {
+		chdir($targetDir);
+	} else {
+		throw new Exception($targetDir . " is not a valid directory");
+	}
+
+	echo syncRepository($cfg);
+}
+
+/**
+ * Perform the necessary commands to sync the folder to the repository.
+ *
+ * It can be assumed that when this function is called, the working directory
+ * is set to the target directory specified in the sink config.
+ *
+ * Returns a string containing the output of the command
+ *
+ */
+function syncRepository($cfg) {
+	$statusCode = -1;
+	$output = array();
+
+	// Let's pull out some configured parameters, maybe use some defaults
+	$remoteName = getFromArray('REMOTE_NAME', $cfg, 'origin');
+	$branchName = getFromArray('GITHUB_BRANCH', $cfg, 'master');
+	$resetMode = getFromArray('RESET_MODE', $cfg, false);
+
+	// These will be the bash commands we execute
+	$cmds = array();
+   
+	// Perform a git checkout of the branch we are working with
+	$cmds[]	= "git checkout $branchName";
+
+	// If they want to reset, do so before pulling
+	if ($resetMode) {
+		$cmds[]	= "git reset --$resetMode HEAD";
+	}
+
+	// The big pull - grab using the remote and branch
+	$cmds[]	= "git pull $remoteName $branchName";
+
+	foreach ($cmds as $cmd) {
+		echo "Executing: $cmd\n";
+		exec(escapeshellcmd($cmd) . " 2>&1", $output, $statusCode);
+
+		if ($statusCode !== 0) {
+			throw new Exception(implode("\n", $output));
+		}
+	}
+
+	return implode("\n", $output);
+}
+
+/**
+ * Check the SHA header against the configured secret phrase.
+ *
+ * Assume that the secret phrase has been changed and that the GitHub header is present.
+ *
+ */
+function checkSecretKey($payload) {
+	$shaHash = 'sha1=' . hash_hmac( 'sha1', $payload, getConfig('SECRET_PHRASE'), false );
+	return strcmp($shaHash, $_SERVER['HTTP_X_HUB_SIGNATURE']) === 0;
 }
 
 /**
@@ -75,5 +158,13 @@ function getFromArray($key, $arr, $default) {
 		return $arr[$key];
 	}
 	return $default;
+}
+
+/**
+ * Kill execution while also setting an HTTP status code of 500 - so GitHub knows it failed
+ */
+function die500($msg) {
+	header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+	die($msg);
 }
 ?>
